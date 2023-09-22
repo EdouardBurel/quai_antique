5b0c4c0350c362febe812698b8de9734d2eb2f51
Edouard Burel <bureledo@gmail.com>
2023-09-22 12:19:22 +0200

    solve conflict

--- templates/headerBootstrap.php
+++ templates/headerBootstrap.php
@@ -1,13 +1,11 @@
 <!doctype html>
-   <html lang="fr" class="htmlForm">
-   <head>
+<html lang="fr" class="htmlForm">
+
+<head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-<<<<<<< HEAD
       <title>Quai Antique - Restaurant Chef Arnaud Michaud </title>
-=======
       <title>Quai Antique</title>
->>>>>>> 4f22ba191a6d1ecb562ee288d0d1fbad8390bae8
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -15,4 +13,4 @@
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Cinzel&family=Gloock&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="/assets/css/styles.css">
-   </head>
+</head>
